243fe5b21e439b313799ae683cf34d0df4e6dbab
Unmilan <unmilanm@gmail.com>
2020-08-30 02:01:33 +0530

    Set-up main app list view

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -15,7 +15,8 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        categoryTable.dataSource = self
+        categoryTable.delegate = self//Here we are saying that the datasource and delegate is this self. This is required for the UITableViewDataSource and UITableViewDelegate to work.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,9 +27,10 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {//dequeue reusable cell sets the pattern for reusable cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {//dequeue reusable cell sets the pattern for reusable cell. Also here the "CategoryCell" is an identifier for our CategoryCell or rather TableCell in general. We gotta change the identifier of our TableCell/CategoryCell in the Main storyboard in the TableCell attributes tab for this to work.
             
             let category = DataService.instance.getCategories()[indexPath.row]//Here we are returning an array from a function. We are first grabbing the categories and then we are grabbing the specific item from the category.
+            
             cell.updateViews(category: category)
             return cell
             
