9ffd107790d4dde004d47299fea045d01f092813
Unmilan <unmilanm@gmail.com>
2020-08-30 12:11:52 +0530

    Designed a Product view cell for displaying products in a category.

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Unmilan on 30/08/20.
+//  Copyright © 2020 Unmilan. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
